c3a668a46c8f075fd731fc9c8c13b2f491eddb85
shawn <shawnxiao@easyops.cn>
2019-01-19 17:13:02 +0800

    revote(master): reset

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { MockDateComponent } from './mock-date/mock-date.component';
-import { MockTimerComponent } from './mock-timer/mock-timer.component';
-import { TestServiceComponent } from './test-service/test-service.component';
-
 @NgModule({
   declarations: [
     AppComponent,
-    MockDateComponent,
-    MockTimerComponent,
-    TestServiceComponent
   ],
   imports: [
     BrowserModule
